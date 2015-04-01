bf53d0fc8729b01ead23da1a193ee183d6e81f8b
Aelar <benja.piat@gmail.com>
2015-04-01 18:28:45 +0200

    gtrfesdcxw

--- src/Connection/Connection.java
+++ src/Connection/Connection.java
@@ -1,7 +1,20 @@
 package Connection;
+import twitter4j.*;
+
+import java.util.List;
 
 /**
  * Created by aelar on 01/04/15.
  */
 public class Connection {
+
+    public static void main(String[] args) throws TwitterException {
+        Twitter twitter = TwitterFactory.getSingleton();
+        Query query = new Query("#swag ");
+        QueryResult result = twitter.search(query);
+        for (Status status : result.getTweets()) {
+            System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+        }
+    }
+
 }
